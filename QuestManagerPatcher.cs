a5683e36c2111319fb60cf58a13396824c600d89
ChromeWing <ifeldsch@gmail.com>
2022-07-23 23:31:33 -0500

    Fixed mistake with adjusting Quest Instances for the mod (it was instead doing it a lot more often at the wrong times)

--- QuestManagerPatcher.cs
+++ QuestManagerPatcher.cs
@@ -327,8 +327,7 @@ namespace GhostloreAP
     {
         static bool Prefix(QuestInstance questInstance, ref Quest[] ___quests, ref QuestInstance[] ___questInstances)
         {
-            if (questInstance == null) { return false; }
-            if(QuestFactory.instance==null || QuestFactory.instance.CheckInitializedQuestInstances()) { return true; }
+            if(questInstance!=null) { return true; }
             GLAPModLoader.DebugShowMessage("we're in it to win it");
 
             List<Quest> _quests = new List<Quest>(___quests);
@@ -377,18 +376,7 @@ namespace GhostloreAP
 
     }
 
-    [HarmonyPatch(typeof(QuestManager), nameof(QuestManager.IsLocationUnlocked))]
-    public class IsLocationUnlockedPatcher
-    {
-        static void Postfix(GameLocation location,GameLocationAttributes locationsReached, bool __result,QuestInstance[] ___questInstances)
-        {
-            QuestInstance qi = ___questInstances.FirstOrDefault((QuestInstance c) => c.Quest == location.QuestRequirement);
-            if(qi == null) { return; }
-            var currentStage_=Traverse.Create(qi).Field("currentStage").GetValue<int>();
-            Quest q = Traverse.Create(qi).Field("quest").GetValue<Quest>();
-            GLAPModLoader.DebugShowMessage("DEBUG UNLOCKS:result="+__result+"location="+location.name+"location.QuestRequirement Null?"+(location.QuestRequirement==null)+",locationsReached="+((int)locationsReached)+"locationAttrib="+((int)location.Attributes)+",questInstanceNull?"+(qi==null)+",questInstance.Active?"+(qi!=null && qi.Active)+",questInstance.Completed?"+(qi != null && qi.Completed)+",currentStage="+(currentStage_)+",stagesLength="+q.Stages.Length);
-            GLAPModLoader.SaveLog();
-        }
 
-    }
+
+
 }
