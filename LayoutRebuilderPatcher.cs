61e02fe25be94c8dbb8d3d5caebba1fa8c1923d0
ChromeWing <ifeldsch@gmail.com>
2022-10-22 17:16:09 -0500

    potential fix to crash report ghostlore was making sometimes with layoutrebuilder (still needs to be pushed to workshop mod)

---
+++ LayoutRebuilderPatcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace GhostloreAP
+{
+    //this is one of the only patches I think I ever had to make to something Unity engine related...
+    [HarmonyPatch(typeof(LayoutRebuilder), "PerformLayoutCalculation")]
+    public class LayoutRebuilderPatcher
+    {
+        static bool Prefix(RectTransform rect)
+        {
+            List<Component> list = CollectionPool<List<Component>, Component>.Get();
+            rect.GetComponents(typeof(ILayoutElement), list);
+            if(list == null)
+            {
+                //list is somehow null, don't allow LayoutRebuilder.StripDisabledBehavioursFromList() to get called and break.
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(LayoutRebuilder), "PerformLayoutControl")]
+    public class LayoutRebuilderPatcher2
+    {
+        static bool Prefix(RectTransform rect)
+        {
+            List<Component> list = CollectionPool<List<Component>, Component>.Get();
+            rect.GetComponents(typeof(ILayoutController), list);
+            if (list == null)
+            {
+                //list is somehow null, don't allow LayoutRebuilder.StripDisabledBehavioursFromList() to get called and break.
+                return false;
+            }
+            return true;
+        }
+    }
+}
